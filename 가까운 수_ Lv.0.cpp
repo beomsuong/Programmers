3ce1e26e1dcbdd256fc6161457eda455b5e11800
beomsuong <129273417+beomsuong@users.noreply.github.com>
2023-03-30 02:19:19 +0900

    Update 가까운 수_ Lv.0.cpp

--- 가까운 수_ Lv.0.cpp
+++ 가까운 수_ Lv.0.cpp
@@ -1,15 +1,21 @@
 #include <string>
 #include <vector>
-
+#include<algorithm>
 using namespace std;
 
 int solution(vector<int> array, int n) {
     int answer = 9999999;
-    int w = 0;
-    for (int i = 0;i < array.size();i++)
+    int w = 99999999;
+    sort(array.begin(), array.end());
+    for (int i = array.size()-1;i>=0;i--)
     {
-        answer = min(answer, abs(array[i] - n));
-        w = array[i];
+        if (abs(array[i] - n) <= answer)
+        {
+            answer = abs(array[i] - n);
+
+              w = array[i];
+        }
+         
     }
     return w;
 }
