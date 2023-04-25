227fe8af5a4b888a2f77fa502a97739e580c5964
beomsuong <129273417+beomsuong@users.noreply.github.com>
2023-04-25 22:48:16 +0900

    Create 문자열 겹쳐쓰기.cpp

---
+++ Lv.0/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    string answer = "";
+    for(int i=0;i<s;i++){
+        answer+=my_string[i];
+    }
+      
+        answer+=overwrite_string;
+  
+     for(int i=overwrite_string.size()+s;i<my_string.size();i++){
+        answer+=my_string[i];
+    }
+    return answer;
+}
