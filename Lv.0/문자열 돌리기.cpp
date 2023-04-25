fecad63cb65e0ddf2e1a7f8ae07b501f581fda4f
beomsuong <129273417+beomsuong@users.noreply.github.com>
2023-04-25 22:35:45 +0900

    Create 문자열 돌리기.cpp

---
+++ Lv.0/문자열 돌리기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    for(auto i: str)
+    {
+        cout<<i<<endl;
+            
+    }
+    return 0;
+}
