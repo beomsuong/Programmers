f71b8a51dcc7d19e85cf8051b26455d179119cef
beomsuong <129273417+beomsuong@users.noreply.github.com>
2023-04-28 04:02:22 +0900

    Create 홀수 vs 짝수.cpp

---
+++ Lv.0/홀수 vs 짝수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    int a = 0, b = 0;
+    for (int i = 0;i < num_list.size();i++) {
+        if (i % 2 == 0)
+            a += num_list[i];
+        else
+            b += num_list[i];
+    }
+    return a>b?a:b;
+}
