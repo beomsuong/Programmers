e4d1b190e60f4a1f17958689d0762253b3d8f73b
beomsuong <129273417+beomsuong@users.noreply.github.com>
2023-04-28 15:34:23 +0900

    Create 배열의 길이에 따라 다른 연산하기.cpp

---
+++ Lv.0/배열의 길이에 따라 다른 연산하기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int n) {
+    vector<int> answer;
+    for (int i = 0; i < arr.size();i++) {
+        if ((arr.size() - 1) % 2 == 0 && i % 2 == 0)
+            answer.push_back(arr[i] + n);
+        else if ((arr.size() - 1) % 2 != 0 && i % 2 != 0)
+            answer.push_back(arr[i] + n);
+        else
+            answer.push_back(arr[i]);
+    }
+    return answer;
+}
