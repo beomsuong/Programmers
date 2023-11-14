1398a773b1ad0160ef20ba31b5ad7d46dde04a40
beomsuong <129273417+beomsuong@users.noreply.github.com>
2023-11-14 19:49:43 +0900

    Create 폰켓몬.cpp

---
+++ Lv.1/폰켓몬.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int answer = nums.size() / 2;
+    sort(nums.begin(), nums.end());
+    nums.erase(unique(nums.begin(), nums.end()), nums.end());
+    return nums.size() < answer ? nums.size() : answer;
+}
