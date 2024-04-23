a903dea775cad9a750ddeaeef1a4e7dd7f2e4513
beomsuong <129273417+beomsuong@users.noreply.github.com>
2024-04-23 10:05:16 +0900

    Create 루시와 엘라 찾기.sql

---
+++ MYSQL/Lv.1/루시와 엘라 찾기.sql
@@ -0,0 +1,1 @@
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE from ANIMAL_INS  where NAME in ('Ella')or NAME in ('Lucy')or NAME in ('Pickle') or NAME in ('Rogan')or NAME in ('Sabrina')or NAME in ('Mitty')
